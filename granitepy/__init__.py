8782f0376cf113eb255bdbf28fabe761a653d122
MyNameBeMrRandom <mynamebemrrandom@gmail.com>
2020-02-04 21:58:44 +0000

    reodered imports

--- granitepy/__init__.py
+++ granitepy/__init__.py
@@ -4,9 +4,9 @@ Made by twitch#7443 and MrRandom#9258
 __version__ = "0.1.0a"
 
 from .client import Client
-from .events import *
-from .exceptions import *
-from .filters import *
-from .node import Node
-from .objects import *
 from .player import Player
+from .node import Node
+from .exceptions import *
+from .events import *
+from .filters import *
+from .objects import *
