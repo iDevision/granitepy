6ba3674271e7619d0b02926b8fb6f25b3936ba48
MyNameBeMrRandom <mynamebemrrandom@gmail.com>
2020-02-04 21:59:30 +0000

    raise errors on invalid values and added reprs

--- granitepy/filters.py
+++ granitepy/filters.py
@@ -1,3 +1,6 @@
+from . import exceptions
+
+
 class Filter:
 
     def __init__(self):
@@ -7,8 +10,8 @@ class Filter:
 class Karaoke(Filter):
 
     def __init__(self, *, level: float, mono_level: float, filter_band: float, filter_width: float):
-
         super().__init__()
+
         self.level = level
         self.mono_level = mono_level
         self.filter_band = filter_band
@@ -18,21 +21,22 @@ class Karaoke(Filter):
                                     "monoLevel": self.mono_level,
                                     "filterBand": self.filter_band,
                                     "filterWidth": self.filter_width}}
+
     def __repr__(self):
-        return f"GraniteFilterKaraoke level={level} monoLevel={mono_level} filterBand={filter_band} filterWidth={filter_width}"
+        return f"GraniteFilterKaraoke level={self.level} monoLevel={self.mono_level} filterBand={self.filter_band} filterWidth={self.filter_width}"
 
 
 class Timescale(Filter):
 
     def __init__(self, *, speed: float, pitch: float, rate: float):
-
         super().__init__()
+
         if speed < 0:
-            raise ValueError("Timescale speed must be more than 0.")
+            raise exceptions.FilterInvalidArgument("Timescale speed must be more than 0.")
         if pitch < 0:
-            raise ValueError("Timescale pitch must be more than 0.")
+            raise exceptions.FilterInvalidArgument("Timescale pitch must be more than 0.")
         if rate < 0:
-            raise ValueError("Timescale rate must be more than 0.")
+            raise exceptions.FilterInvalidArgument("Timescale rate must be more than 0.")
 
         self.speed = speed
         self.pitch = pitch
@@ -41,25 +45,29 @@ class Timescale(Filter):
         self.payload = {"timescale": {"speed": self.speed,
                                       "pitch": self.pitch,
                                       "rate": self.rate}}
+
     def __repr__(self):
-        return f"GraniteFilterTimescale speed={speed} pitch={pitch} rate={rate}"
+        return f"GraniteFilterTimescale speed={self.speed} pitch={self.pitch} rate={self.rate}"
 
 
 class Tremolo(Filter):
 
     def __init__(self, *, frequency: float, depth: float):
-
         super().__init__()
+
         if frequency < 0:
-            raise ValueError("Tremolo depth must be more than 0.")
+            raise exceptions.FilterInvalidArgument("Tremolo frequency must be more than 0.")
         if 0 > depth >= 1:
-            raise ValueError("Tremolo depth must be between 0 and 1.")
+            raise exceptions.FilterInvalidArgument("Tremolo depth must be between 0 and 1.")
 
         self.frequency = frequency
         self.depth = depth
 
         self.payload = {"tremolo": {"frequency": self.frequency,
                                     "depth": self.depth}}
+
+    def __repr__(self):
+        return f"GraniteFilterTremolo frequency={self.frequency} depth={self.depth}"
 
 
 class Vibrato(Filter):
@@ -68,12 +76,15 @@ class Vibrato(Filter):
 
         super().__init__()
         if 0 > frequency >= 14:
-            raise ValueError("Vibrato frequency must be between 0 and 14.")
+            raise exceptions.FilterInvalidArgument("Vibrato frequency must be between 0 and 14.")
         if 0 > depth >= 1:
-            raise ValueError("Vibrato depth must be between 0 and 1.")
+            raise exceptions.FilterInvalidArgument("Vibrato depth must be between 0 and 1.")
 
         self.frequency = frequency
         self.depth = depth
 
         self.payload = {"vibrato": {"frequency": self.frequency,
                                     "depth": self.depth}}
+
+    def __repr__(self):
+        return f"GraniteFilterVibrato frequency={self.frequency} depth={self.depth}"
