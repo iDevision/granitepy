82c57fe4ae2559b8be032dcd74d1e74b37f417b2
MyNameBeMrRandom <mynamebemrrandom@gmail.com>
2020-04-04 21:56:04 +0100

    updated setup.py file

--- setup.py
+++ setup.py
@@ -5,29 +5,35 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 with open("README.md") as f:
-    long_description = f.read()
+    readme = f.read()
 
 
 setuptools.setup(
     name="granitepy",
-    version="0.2.0a",
     author="MrRandom#9258 and twitch 🔋#7443",
+    version="0.2.0a",
     url="https://github.com/iDevision/granitepy",
+    packages=setuptools.find_packages(),
     licence="AGPL-3.0",
     description="A python client for the audio provider Andesite for use with discord.py.",
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type="text/markdown",
-    keywords=['andesite', 'granitepy'],
-    packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    python_requires='>=3.6',
+    extra_require=None,
     classifiers=[
         "Framework :: AsyncIO",
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries',
         "Topic :: Internet"
-    ]
+    ],
+    python_requires='>=3.6',
+    keywords=['andesite', 'granitepy', "discord.py"],
 )
