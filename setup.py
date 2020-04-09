db305c15c0580c256a9fc9df11442fe8d504a4bb
MyNameBeMrRandom <mynamebemrrandom@gmail.com>
2020-04-09 02:39:37 +0100

    version bump to 0.4.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md") as f:
 setuptools.setup(
     name="granitepy",
     author="MrRandom#9258 and twitch 🔋#7443",
-    version="0.4.0",
+    version="0.4.1",
     url="https://github.com/iDevision/granitepy",
     packages=setuptools.find_packages(),
     licence="MIT",
